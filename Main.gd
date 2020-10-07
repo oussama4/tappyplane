64575f619569d4d5737b985de34809c8b7a8adcc
oussama zouaki <oussamazouaki4@gmail.com>
2020-10-07 18:56:54 +0100

    add rock down

--- Main.gd
+++ Main.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 export (PackedScene) var Rock
+export (PackedScene) var RockDown
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +17,17 @@ func _ready():
 
 
 func _on_RockTimer_timeout():
-	var rock = Rock.instance()
+	var rock = create_rock()
 	add_child(rock)
-	rock.position = Vector2(rand_range(800, 1500), rand_range(130, 230))
+
+
+func create_rock():
+	var r = randi() % 2
+	if r == 0:
+		var rock = Rock.instance()
+		rock.position = Vector2(rand_range(800, 1400), rand_range(130, 230))
+		return rock
+	else:
+		var rock = RockDown.instance()
+		rock.position = Vector2(rand_range(800, 1400), rand_range(0, 100))
+		return rock
